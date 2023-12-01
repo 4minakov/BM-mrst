a848122eb389d3c9cc01e09d6deebfec88234ec5
Oystein Klemetsdal <oystein.klemetsdal@gmail.com>
2023-12-01 13:23:55 +0100

    Set up parameter tuning with comments/explanations

--- devscript.m
+++ devscript.m
@@ -1,3 +1,6 @@
+%% Model tuning of Baia Mare
+
+% Add modules
 mrstModule add baia-mare
 mrstModule add test-suite
 mrstModule add ad-core ad-props compositional
@@ -5,73 +8,89 @@ mrstModule add geothermal
 mrstModule add upr
 mrstModule add optimization
 mrstModule add mrst-gui
-
+% Command window output
 mrstVerbose on
-
+% Load datasets
 loadData = @(name) load(fullfile(mrstPath('baia-mare'), name));
 
 %% Set up test case
 setup = TestCase('baia_mare_geothermal_2', 'readGridFromDisk', true);
 setup.plot();
 
-%% Set up and simulate problem
+%% Simulate base case
 problem = setup.getPackedSimulationProblem();
 simulatePackedProblem(problem, 'restartStep', nan);
 
-%% Get results
-[~, states, reports] = getPackedSimulatorOutput(problem);
+%% Get base case results
+[~, states] = getPackedSimulatorOutput(problem);
+T0 = states{end}.T;
 setup.plot(states);
 
-%% Set up optimization problem (TBC)
-% set up 'matrix' for parameter options for easier editing. The specific
-% limits set for the various parameters influences the tuning/optimization 
-% procedure to a large extent
-
+%% Set up optimization problem
+% Initial guess
 setup0 = setup;
+% Set permeability as function
 setup0.model.rock.permVal = setup.model.rock.perm;
 setup0.model.rock.perm    = @(p,T) setup0.model.rock.permVal + p.*0;
-setup0.model = setup0.model.setupOperators();
-
+setup0.model              = setup0.model.setupOperators();
+% Make partition vector defining regions with equal parameters. Adjust
+% this to a suitalble partition
 G = setup0.model.G;
-pcell = G.cells.tag.crus_cell.*1 + G.cells.tag.sed1_cell.*2 + G.cells.tag.sed2_cell.*3;
-pface = max(pcell(setup0.model.operators.N), [], 2);
-
-pv = nan(max(pcell),1);
-pv(pcell) = setup0.model.operators.pv;
-
+pcell = G.cells.tag.crus_cell.*1 ...
+      + G.cells.tag.sed1_cell.*2 ...
+      + G.cells.tag.sed2_cell.*3;
+setup.plot(pcell);
+% Set up parameters for tuning
+% boxLimits: Minimum/maximum allowed parameter value
 parameters = [];
-parameters = addParameter(parameters, setup0, ...
-    'name', 'permeability', ...
-    'location', {'rock', 'perm'}, ...
-    'belongsTo', 'model', ...
-    'setfun', @(varargin) setPermeability(varargin{:}), ...
-    'scaling', 'log', ...
-    'relativeLimits', [1e-4, 1e4], ...
-    'lumping', pcell ...
-);
-parameters = addParameter(parameters, setup0, ...
-    'name', 'porosity', ...
-    'location', {'rock', 'poro'}, ...
-    'belongsTo', 'model', ...
-    'setfun', @(varargin) setPorosity(varargin{:}), ...
-    'scaling', 'linear', ...
-    'boxLims', [0.01, 0.8], ...
-    'lumping', pcell ...
-);
-parameters = addParameter(parameters, setup0, ...
-    'name', 'radiogenicHeatFluxDensity', ...
-    'location', {'radiogenicHeatFluxDensity'}, ...
-    'belongsTo', 'model', ...
-    'scaling', 'linear', ...
-    'boxLims', [0.001, 100]*micro*watt/meter^3, ...
-    'lumping', pcell ...
-);
 
+% Permeability
+%-------------------------------------------------------------------------%
+permLim = [0.01, 2000; 0.01, 2000; 0.01, 2000]*milli*darcy;
+parameters = addParameter(parameters, setup0, ...
+    'name'     , 'permeability'                          , ...
+    'location' , {'rock', 'perm'}                        , ...
+    'belongsTo', 'model'                                 , ...
+    'setfun'   , @(varargin) setPermeability(varargin{:}), ...
+    'scaling'  , 'log'                                   , ...
+    'boxLims'  , permLim                                 , ...
+    'lumping'  , pcell                                     ...
+);
+%-------------------------------------------------------------------------%
+
+% Porosity
+%-------------------------------------------------------------------------%
+poroLim = [0.001, 0.8; 0.001, 0.8; 0.001, 0.8];
+parameters = addParameter(parameters, setup0, ...
+    'name'     , 'porosity'                          , ...
+    'location' , {'rock', 'poro'}                    , ...
+    'belongsTo', 'model'                             , ...
+    'setfun'   , @(varargin) setPorosity(varargin{:}), ...
+    'scaling'  , 'linear'                            , ...
+    'boxLims'  , poroLim                             , ...
+    'lumping'  , pcell                                 ...
+);
+%-------------------------------------------------------------------------%
+
+% Radiogenic heat flux density
+%-------------------------------------------------------------------------%
+qLim = [0.001, 100; 0.001, 100; 0.001, 100]*micro*watt/meter^3;
+parameters = addParameter(parameters, setup0, ...
+    'name'     , 'radiogenicHeatFluxDensity'  , ...
+    'location' , {'radiogenicHeatFluxDensity'}, ...
+    'belongsTo', 'model'                      , ...
+    'scaling'  , 'linear'                     , ...
+    'boxLims'  , qLim                         , ...
+    'lumping'  , pcell                          ...
+);
+%-------------------------------------------------------------------------%
+
+% Get parameter vector
 pvec = getScaledParameterVector(setup0, parameters);
 
-%% Model calibration Quasi-Newton
-% make handle
-loadData = @(name) load(fullfile(mrstPath('baia-mare'), name));
+%% Load reference data
+% The observed temperature and the corresponding grid cells must be
+% provided to the temperatureMismatch (see below)
 data     = loadData('Wells1.mat'    ); Wells1 = data.Wells;
 data     = loadData('Wells2.mat'    ); DemWel = data.DemWel;
 data     = loadData('DemWell1-6.mat'); DemWelTVal = data;
@@ -82,133 +101,83 @@ keep = ismember(Wells1.Var3, include);
 Wells1 = Wells1(keep,:);
 
 Tobs = [Wells1.zo, Wells1.z1000, Wells1.z2000, Wells1.z2500];
-z0 = Wells1.Elevation;
-z  = z0 -[0 1000 2000 2500];
+Tobs0 = Tobs;
+ztop = Wells1.Elevation;
+z    = ztop -[0 1000 2000 2500];
+z0   = z;
 
 x = repmat(Wells1.profdstA05, 1, 4);
 
 Tobs = reshape(Tobs', [], 1);
-x = reshape(x', [], 1);
-z = reshape(z', [], 1);
-x = [x,z];
+x    = reshape(x', [], 1);
+z    = reshape(z', [], 1);
+x    = [x,z];
 cells = findEnclosingCell(setup.model.G, x);
-
+% Only match againts values measured inside simulation grid
 inside = cells > 0;
-cells = cells(inside);
-Tobs = Tobs(inside);
 
-%%
+cells = cells(inside);
+Tobs  = Tobs(inside);
+
+%% Perform model Quasi-Newton model calibration
 close all
+% Set up mismatch function
 mismatchFn = @(model, states, schedule, states_ref, compDer, tstep, state) ...
     temperatureMismatch(model, states, schedule, Tobs, cells, ...
-                   'computePartials', compDer, 'tstep', tstep, ...
-                   'state', state, 'from_states', false);
-objh = @(p) evaluateMatch(p, mismatchFn, setup0, parameters, statesRef);
+    'computePartials', compDer, 'tstep', tstep, ...
+        'state', state, 'from_states', false);
+objh = @(p) evaluateMatch(p, mismatchFn, setup0, parameters, []);
 % The calibration can be improved by taking a large number of iterations,
-% but here we set a limit of 30 iterations
+% but here we set a limit of 20
 [objVal, pvecOpt, history] = unitBoxBFGS(pvec, objh, 'objChangeTol', 1e-5, ...
                                   'maxIt', 20, 'logPlot', true);
 
-% %% Model calibration Levenberg-Marquard (using full Jacobian)
-% mismatchFn2 = @(model, states, schedule, states_ref, compDer, tstep, state) ...
-%     temperatureMismatch(model, states, schedule, Tobs, cells, ...
-%         'computePartials', compDer, 'tstep', tstep,...
-%         'state', state, 'from_states', false, 'mismatchSum', false);
-% objh2 = @(p) evaluateMatchSummands(p, mismatchFn2, setup0, parameters, statesRef);
-% % The calibration can be improved by taking a large number of iterations,
-% % but here we set a limit of 30 iterations
-% [v2, p_opt2, history2] = unitBoxLM(pvec, objh2, 'maxIt', 20);
-
-%%
+%% Make new setup with tuned parameters and simulate
 setupOpt = updateSetupFromScaledParameters(setup, parameters, pvecOpt); 
 problemOpt = setupOpt.getPackedSimulationProblem();
 simulatePackedProblem(problemOpt, 'restartStep', 1);
                               
-%%
-[~, states, reports] = getPackedSimulatorOutput(problemOpt);
-setupOpt.plot(states);
+%% Load results from tuned model
+[~, statesOpt, reports] = getPackedSimulatorOutput(problemOpt);
+TOpt = convertToCelcius(statesOpt{end}.T);
 
-%%
-FF = scatteredInterpolant;
-FF.Points = setup.model.G.cells.centroids; 
-FF.ExtrapolationMethod='none';
-FF.Values = convertToCelcius(states{end}.T);
-
-%%
-DemWelTVal = loadData('DemWell1-6.mat');
-data1 = loadData('Wells1.mat'); Wells1 = data1.Wells;
-data2 = loadData('Wells2.mat'); DemWel = data2.DemWel;
-
-figure,  hold on,
-
-plot(Wells1.profdstA05,Wells1.Elevation,'^','MarkerFaceColor','r','MarkerEdgeColor','k','MarkerSize',10)
-text(Wells1.profdstA05,Wells1.Elevation,Wells1.Var3)
-
-plot(DemWel.profdstA05,DemWel.Elevation,'o','MarkerFaceColor','r','MarkerEdgeColor','k','MarkerSize',10)
-text(DemWel.profdstA05,DemWel.Elevation,DemWel.Site), ylim([-9e3 2e3])
-
-%%
-n = 100;
-
-% z = DemWelTVal.well2(:,2)-DemWel.Elevation(4);
-
-z = -[0 1000 2000 2500]';
-x = repmat(DemWel.profdstA05(5), numel(z), 1);  
-
-x = [x, z];
-cells = findEnclosingCell(setup.model.G, x);
-
+%% Plot match
 close all
-plotGrid(setup.model.G, cells);
-plotGrid(setup.model.G, 'FaceColor', 'none');
-setup.setAxisProperties(gca);
+fg = setup.figure();
+plotGrid(setup.model.G); setup.setAxisProperties(gca), hold on,
 
+colors = lines(numel(include));
+for i = 1:numel(include)
+    
+    color = colors(i,:);
+    figure(fg),
+    plot(Wells1.profdstA05(i), Wells1.Elevation(i), ...
+        'v', 'MarkerFaceColor', color, 'MarkerEdgeColor', 'k', 'MarkerSize', 10)
+    text(Wells1.profdstA05(i), Wells1.Elevation(i), Wells1.Var3{i})
+    
+    ft = figure(); hold on
+    To = Tobs0(i,:);
+    z  = z0(i,:);
+    plot(To, z, 'o', 'Color', color, 'LineWidth', 2);
 
-%%
+    x = Wells1.profdstA05(i);
+    n = 25;
+    t = linspace(0,1,n)';    
+    x = interp1([0,1]', [x,z(1); x, z(end)], t);
+    
+    cells = findEnclosingCell(setup.model.G, x);
+    cells = cells(cells > 0);
+    cells = rlencode(cells,1);
+    
+    zg = setup.model.G.cells.centroids(cells,2);
+    plot(TOpt(cells), zg, 'LineWidth', 2, 'Color', color);
+    axis tight, grid on, box on
+    legend({'Measured', 'Simulated'});    
+    figure(fg), plotGrid(setup.model.G, cells, 'faceColor', color)
+    
+end
 
-zz = linspace(-1500,600,20)';
-
-figure(40),clf
-
-subplot(2,4,1), plot(DemWelTVal.well2(:,1),DemWelTVal.well2(:,2)-DemWel.Elevation(2),'b'),set(gca,'Ydir','reverse'), title(DemWel.Site(2)),%ylim([-1000 1000]),xlim([10 30])
-tt = FF(DemWel.profdstA05(2) + zz*0, zz); 
-hold on, plot(tt,-zz,'r'), legend('Data', 'Model')
-
-subplot(2,4,2), plot(DemWelTVal.well3(:,1),DemWelTVal.well3(:,2)-DemWel.Elevation(3),'b'),set(gca,'Ydir','reverse'), title(DemWel.Site(3)),ylim([-1000 1000]),xlim([10 30])
-tt = FF(DemWel.profdstA05(3) + zz*0, zz); 
-hold on, plot(tt,-zz,'r'), legend('Data', 'Model')
-
-subplot(2,4,3), plot([Wells1.zo(2),Wells1.z1000(2),Wells1.z2000(2),Wells1.z2500(2)],...
-    [0 1000 2000 2500],'b'),set(gca,'Ydir','reverse'), title(Wells1.Var3(2)),%ylim([-1000 1000]),xlim([10 150])
-tt = FF(Wells1.profdstA05(2) + zz*0, zz); 
-hold on, plot(tt,-zz,'r'), legend('Data', 'Model')
-
-subplot(2,4,4), plot([Wells1.zo(1),Wells1.z1000(1),Wells1.z2000(1),Wells1.z2500(1)],...
-    [0 1000 2000 2500],'b'),set(gca,'Ydir','reverse'), title(Wells1.Var3(1)),%ylim([-1000 1000]),xlim([10 150])
-hold on, plot(DemWelTVal.well6(:,1),DemWelTVal.well6(:,2)-DemWel.Elevation(6),'b'),
-tt = FF(Wells1.profdstA05(1) + zz*0, zz); 
-hold on, plot(tt,-zz,'r'), legend('Data', 'Model')
-
-set(gca,'Ydir','reverse'), title(DemWel.Site(6)),%ylim([-1000 1000]),xlim([10 150])
-subplot(2,4,5), plot([Wells1.zo(6),Wells1.z1000(6),Wells1.z2000(6),Wells1.z2500(6)],...
-    [0 1000 2000 2500],'b'),set(gca,'Ydir','reverse'), title(Wells1.Var3(6)),%ylim([-1000 1000]),xlim([10 150])
-tt = FF(Wells1.profdstA05(6) + zz*0, zz); 
-hold on, plot(tt,-zz,'r'), legend('Data', 'Model')
-
-subplot(2,4,6), plot([Wells1.zo(3),Wells1.z1000(3),Wells1.z2000(3),Wells1.z2500(3)],...
-    [0 1000 2000 2500],'b'),set(gca,'Ydir','reverse'), title(Wells1.Var3(3)),%ylim([-1000 1000]),xlim([10 150])
-tt = FF(Wells1.profdstA05(3) + zz*0, zz); 
-hold on, plot(tt,-zz,'r'), legend('Data', 'Model')
-
-subplot(2,4,7), plot([Wells1.zo(4),Wells1.z1000(4),Wells1.z2000(4),Wells1.z2500(4)],...
-    [0 1000 2000 2500],'b'),set(gca,'Ydir','reverse'), title(Wells1.Var3(5)),%ylim([-1000 1000]),xlim([10 150])
-hold on, plot([Wells1.zo(5),Wells1.z1000(5),Wells1.z2000(5),Wells1.z2500(5)],[0 1000 2000 2500],'b'),
-tt = FF(Wells1.profdstA05(5) + zz*0, zz); 
-hold on, plot(tt,-zz,'r'), 
-plot(DemWelTVal.well4(:,1),DemWelTVal.well4(:,2)-DemWel.Elevation(4),'b'),
-plot(DemWelTVal.well5(:,1),DemWelTVal.well5(:,2)-DemWel.Elevation(5),'b')
-
-%%
+%% Setters for permeability and porosity
 function s = setPorosity(s, varargin)
     poro = varargin{end};
     s.rock.poro = poro;
